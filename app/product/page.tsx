683d17443f3383cf86268d1ef66c25644d9d2aa5
Sayuru Akash Amarasinghe <sayuru555@gmail.com>
2025-11-17 10:30:56 +0530

    Update headings in ServicePage and ProductPage components for clarity and impact

--- app/product/page.tsx
+++ app/product/page.tsx
@@ -21,7 +21,7 @@ export default function ProductPage() {
       {/* Header */}
       <div className="flex flex-col pb-6 sm:pb-8 md:pb-16 gap-4 sm:gap-5 md:gap-6">
         <h1 className="font-bold text-2xl sm:text-3xl md:text-4xl lg:text-5xl text-center text-[#8133F1] leading-[120%]">
-          Crafting Digital Masterpieces: Explore Our Work
+          Explore Our Work
         </h1>
         <p className="max-w-[892px] mx-auto font-normal text-xs sm:text-sm md:text-base text-center text-white leading-[140%]">
           Reinforces BYOW’s mission of helping clients bring their visions to
